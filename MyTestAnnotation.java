5392b804c2b409e22fb6d962ff8dd9bf2ca9cfb6
nasty8176u <110005925+nasty8176u@users.noreply.github.com>
2023-12-11 22:22:19 +0900

    Add files via upload

---
+++ MyTestAnnotation.java
@@ -0,0 +1,12 @@
+package ru.fsv67;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyTestAnnotation {
+    int order() default 0;
+}
